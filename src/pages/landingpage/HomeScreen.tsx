84ba47b75244d249ee3f186845991520b2278e98
Dc-Valerian <valerianpedro03@gmail.com>
2023-04-14 18:49:33 +0100

    added a new file in the landing page

--- src/pages/landingpage/HomeScreen.tsx
+++ src/pages/landingpage/HomeScreen.tsx
@@ -3,12 +3,14 @@ import Home from "./Home"
 import Connect from "./Connect"
 import Plan from './Plan'
 import Payment from "./Payment"
+import Modern from './Modern/Modern'
 
 const HomeScreen = () => {
   return (
     <div>
         <Home/>
         <Plan/>
+        <Modern/>
         <Connect/>
         <Payment/>
     </div>
