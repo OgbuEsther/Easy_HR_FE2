bed72317ea4b00eccdf27486fafef81e04c5b3e7
Okwoli-Godwin <okwolig60@gmail.com>
2023-04-18 15:54:26 +0100

    sidebar attendance

--- src/pages/Staffcomp.tsx
+++ src/pages/Staffcomp.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Staffcomp = () => {
   return (
-    <div>Staffcomp</div>
+      <Container>
+          
+    </Container>
   )
 }
 
 export default Staffcomp
+
+const Container = styled.div`
+  width: calc(100vw - 220px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #F5F7FA;
+  overflow: hidden;
+  margin-top: 20px;
+
+  @media screen and (max-width: 1024px) {
+    width: 100vw;
+  }
+`;
