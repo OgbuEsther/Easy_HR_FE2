57a0661667ef7903d34e775da2dcea2dce52bbe9
Okwoli-Godwin <okwolig60@gmail.com>
2023-04-21 02:31:19 +0100

    Tour part done

--- src/pages/Tour/Tour.tsx
+++ src/pages/Tour/Tour.tsx
@@ -125,7 +125,6 @@ const Container = styled.div`
   align-items: center;
   position: fixed;
   justify-content: center;
-  background-color: rgba(0, 0, 0, 0.7);
   position: absolute;
   position: fixed;
   left: 0;
