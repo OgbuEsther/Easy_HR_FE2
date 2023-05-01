3c2514526138273f75e64565665997c8fe74d495
Okwoli-Godwin <okwolig60@gmail.com>
2023-05-01 03:15:50 +0100

    updated

--- src/pages/landingpage/Home.tsx
+++ src/pages/landingpage/Home.tsx
@@ -6,10 +6,12 @@ import img from "../../Assets/hero.jpg"
 const Superhero = () => {
   return (
       <div className='w-full flex justify-center overflow-hidden'>
-          <div className='w-full h-60 flex flex-wrap mt-20 bg-hero bg-no-repeat bg-cover bg-center'>
-        <div className='w-full h-screen flex bg-black bg-opacity-20'>
-          ffufufuf
-              </div>
+          <div className='w-full h-60 flex flex-wrap mt-20 bg-hero bg-no-repeat bg-cover bg-center bg-fixed'>
+        <div className='w-full  flex bg-black bg-opacity-20'>
+          <div className='w-80 text-white mt-20'>
+            Organise your HR and payroll processes efficiently with a modern software that leaves nothing out of the picture, from recruitment to exit.
+          </div>
+        </div>
           </div>
       </div>
   )
