a302a04d85a27fcecaa9197ef7b7f207a396cc22
Okwoli-Godwin <okwolig60@gmail.com>
2023-05-01 04:48:04 +0100

    wanna have a lil sleep

--- src/pages/landingpage/Home.tsx
+++ src/pages/landingpage/Home.tsx
@@ -11,11 +11,11 @@ const Superhero = () => {
           <div className='w-2/5  mt-20 ml-20' >
            <header className="mb-16 group">
   <h1 className="mb-1 font-mono text-4xl text-gray-100 md:text-6xl">
-    Best Leading HR & <br className="block md:hidden" />
-    <span
+    Best Leading HR &  
+     <span
       className="inline-flex h-20 pt-2 overflow-x-hidden animate-type group-hover:animate-type-reverse whitespace-nowrap text-brand-accent will-change-transform"
     >
-      Payroll Software
+        Payroll Software
     </span>
     <span
       className="box-border inline-block w-1 h-10 ml-2 -mb-2 bg-white md:-mb-4 md:h-16 animate-cursor will-change-transform"
