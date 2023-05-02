85f5818bd9ef785d4231ef658d494bbd4087e206
Okwoli-Godwin <okwolig60@gmail.com>
2023-05-02 08:04:33 +0100

    update on hero page

--- src/pages/landingpage/Home.tsx
+++ src/pages/landingpage/Home.tsx
@@ -12,14 +12,10 @@ const Superhero = () => {
            <header className="mb-16 group">
   <h1 className="mb-1 font-mono text-4xl text-gray-100 md:text-6xl">
     Best Leading HR &  
-     <span
-      className="inline-flex h-20 pt-2 overflow-x-hidden animate-type group-hover:animate-type-reverse whitespace-nowrap text-brand-accent will-change-transform"
-    >
+     <span className="inline-flex h-20 pt-2 overflow-x-hidden animate-type group-hover:animate-type-reverse whitespace-nowrap text-brand-accent will-change-transform">
         Payroll Software
     </span>
-    <span
-      className="box-border inline-block w-1 h-10 ml-2 -mb-2 bg-white md:-mb-4 md:h-16 animate-cursor will-change-transform"
-    ></span>
+    <span className="box-border inline-block w-1 h-10 ml-2 -mb-2 bg-white md:-mb-4 md:h-16 animate-cursor will-change-transform"></span>
   </h1>
 </header>
           </div>
