19accbeab964f2c9d65b809b0d64b74f4e5f3664
Okwoli-Godwin <okwolig60@gmail.com>
2023-05-02 09:03:56 +0100

    hero text gotten

--- src/pages/landingpage/Home.tsx
+++ src/pages/landingpage/Home.tsx
@@ -7,8 +7,8 @@ const Superhero = () => {
   return (
       <div className='w-full flex justify-center overflow-hidden'>
         <div className='w-11/12 flex flex-wrap mt-20 bg-white'>
-        <div className='w-3/5 flex flex-col'>
-          <h1 className='text-herotext text-6xl font-semibold'>
+        <div className='w-5/12 flex flex-col'>
+          <h1 className='text-herotext text-6xl font-semibold m-0 leading-tight'>
             Business Payments Made Simple.
           </h1>
             </div>
