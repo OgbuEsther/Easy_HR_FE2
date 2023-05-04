2c583057cc6e97dcce7bef3bcb3325ac3db5d6b4
Okwoli-Godwin <okwolig60@gmail.com>
2023-05-04 23:18:03 +0100

    hero slide gotten

--- src/components/block/Header.tsx
+++ src/components/block/Header.tsx
@@ -482,6 +482,7 @@ const Container = styled.div<{ bd: string; bg: string }>`
   /* font-family: Montserrat; */
   transition: all 350ms ease-in-out; ;
   box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  overflow: hidden;
 `;
 const Wrapper = styled.div`
   width: 92%;
