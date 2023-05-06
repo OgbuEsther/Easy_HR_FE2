1fd8717360a083c925194bd507331a596c7265d5
Okwoli-Godwin <okwolig60@gmail.com>
2023-05-06 22:59:21 +0100

    text added

--- src/pages/landingpage/Home.tsx
+++ src/pages/landingpage/Home.tsx
@@ -24,13 +24,18 @@ const Superhero = () => {
   
   return (
     <div>
-      <Slider {...settings}>
-      <div className='w-full h-screen bg-hero flex justify-center overflow-hidden bg-no-repeat bg-center bg-cover'>
-      <div className='w-full h-screen bg-black bg-opacity-50 '>
+      {/* <Slider {...settings}> */}
+      <div className='w-full z-40 bg-hero flex justify-center overflow-hidden bg-no-repeat bg-center bg-cover'>
+          <div className='w-full bg-black bg-opacity-50 flex-col items-center flex'>
+            <div className='mt-56 w-11/12 text-white'>
+              <div className='w-6/12 text-8xl font-bold'>
+                Payroll & HR Solutions
+              </div>
+            </div>
       </div>
       </div>
 
-      <div className='w-full h-screen bg-righthero flex justify-center overflow-hidden bg-no-repeat bg-center bg-cover'>
+      {/* <div className='w-full h-screen bg-righthero flex justify-center overflow-hidden bg-no-repeat bg-center bg-cover'>
       <div className='w-full h-screen bg-black bg-opacity-50 '>
       </div>
       </div>
@@ -38,8 +43,8 @@ const Superhero = () => {
       <div className='w-full h-screen bg-lefthero flex justify-center overflow-hidden bg-no-repeat bg-center bg-cover'>
       <div className='w-full h-screen bg-black bg-opacity-50 '>
       </div>
-      </div>
-    </Slider>
+      </div> */}
+    {/* </Slider> */}
     </div>
   )
 }
