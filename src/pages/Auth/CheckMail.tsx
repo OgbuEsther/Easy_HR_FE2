d2aa1a265ab7cff6ca0437bcd8373bd0713c1300
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-05-07 16:18:07 +0100

    checking routes

--- src/pages/Auth/CheckMail.tsx
+++ src/pages/Auth/CheckMail.tsx
@@ -1,25 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
-import img from "../../Assets/easy.png"
+// import img from "../../Assets/easy.png"
 import {HiOutlineMail} from "react-icons/hi"
-import {Link} from "react-router-dom"
-import { useAppSelector } from '../../components/global/Store'
 
-const Verification = () => {
-
-    const user = useAppSelector((state) => state.currentUser)
+const CheckMail = () => {
   return (
       <Container>
           <Box>
-              <Img src={img} />
+              {/* <Img src={img} /> */}
               <Card>
                   <Linehold>
                       <Line></Line>
                       <Icons><HiOutlineMail /></Icons>
                       <Line></Line>
                   </Linehold>
-                  <Thanks>THANKS FOR CLICKING!</Thanks>
-                  <Verify><h3>your email has been verified</h3></Verify>
+                  <Thanks>THANKS FOR SIGNING UP!</Thanks>
+                  <Verify><h3>Verify Your E-email Address</h3></Verify>
               </Card>
               <Textspace>
                   <p>Hi,</p>
@@ -27,21 +23,21 @@ const Verification = () => {
                       You're almost ready to get started. Please click on the button below to verify your email and enjoy exclusive service with us
                   </Texthold>
               </Textspace>
-              <Button to = {`${user?._id}/sign-in-admin`}>sign in now!</Button>
+              <Button>VERIFY YOUR EMAIL</Button>
               <Thank>Thanks <br /> The Company's Team</Thank>
           </Box>
     </Container>
   )
 }
 
-export default Verification
+export default CheckMail
 const Thank = styled.div`
     display: flex;
     text-align: center;
     margin-top: 25px;
     color: #505050;
 `
-const Button = styled(Link)`
+const Button = styled.button`
     width: 170px;
     height: 35px;
     display: flex;
