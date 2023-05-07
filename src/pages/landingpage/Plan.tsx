72743b38910918af2118fe0db9005c3d02f866d6
Okwoli-Godwin <okwolig60@gmail.com>
2023-05-07 15:32:26 +0100

    pushed

--- src/pages/landingpage/Plan.tsx
+++ src/pages/landingpage/Plan.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from "styled-components"
 import { Fade } from 'react-awesome-reveal'
+import img from "../../Assets/dollar.png"
 
 const Plan = () => {
   return (
@@ -10,12 +11,58 @@ const Plan = () => {
                   <Par>WE DO MORE FOR YOUR WORLD</Par>
                   <Bolder>All-in-one payroll and HR your business</Bolder>
               </Fade>
+              <Hold>
+                  <Circlehold>
+                      <Circle>
+                      <Img src={img} />
+                      </Circle>
+                      <Pay>Payroll</Pay>
+                  </Circlehold>
+                  <Circle></Circle>
+                  <Circle></Circle>
+                  <Circle></Circle>
+               </Hold>
           </Wrapper>
     </Container>
   )
 }
 
 export default Plan
+const Pay = styled.div`
+    font-size: 16px;
+    margin-top: 20px;
+    margin: 0;
+    color: #001328;
+    font-weight: 800;
+`
+const Circlehold = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+const Img = styled.img`
+    
+`
+const Circle = styled.div`
+    width: 200px;
+    height: 200px;
+    border-radius: 100px;
+    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #fff;
+    :hover{
+        background-color: #dc3545;
+    }
+`
+const Hold = styled.div`
+    width: 100%;
+    margin-top: 70px;
+    display: flex;
+    justify-content: space-between;
+`
 const Bolder = styled.h2`
     margin: 0;
     color: #001328;
