0dd3a310d8d59a1529c68b0f5518a80dfed4182a
Okwoli-Godwin <okwolig60@gmail.com>
2023-05-07 10:42:02 +0100

    second landing page component

--- src/pages/landingpage/Plan.tsx
+++ src/pages/landingpage/Plan.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Plan = () => {
   return (
-      <div className='w-full flex justify-center'>
-          
+      <div className='w-full flex justify-center h-screen bg-heroparagraph'>
+          <div className='w-11/12 h-screen bg-herobutton'>
+              
+          </div>
     </div>
   )
 }
