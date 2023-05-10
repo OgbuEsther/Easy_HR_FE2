82f10a88270358c069f42496f15e97753f54a803
Okwoli-Godwin <okwolig60@gmail.com>
2023-05-10 06:00:51 +0100

    work in progress

--- src/pages/Adminrate.tsx
+++ src/pages/Adminrate.tsx
@@ -1,9 +1,68 @@
 import React from 'react'
+import styled from 'styled-components'
+import { RotatingLines } from 'react-loader-spinner'
+// import Inputdate from "../Inputdate/Inputdate";
 
 const Adminrate = () => {
   return (
-    <div>Adminrate</div>
+      <Container>
+          <Wrapper>
+              <Top>
+                  <Pending><h3>Set Goals</h3><span></span>
+                </Pending>
+
+          <Pending><h3>Rate Staffs</h3><span></span>
+          </Pending>
+              </Top>
+          </Wrapper>
+    </Container>
   )
 }
 
 export default Adminrate
+
+const Pending = styled.div`
+  display: flex;
+  margin: 18px;
+  cursor: pointer;
+  h3{
+    font-weight: 500;
+  font-size: 19px;
+  color: #6c757d;
+  }
+  span{
+    margin-left: 7px;
+    font-weight: 500;
+  font-size: 19px;
+  color: #6c757d;
+  }
+`
+
+const Top = styled.div`
+  width: 100%;
+  display: flex;
+  height: 70px;
+  align-items: center;
+  border-bottom: 1px solid lightgray;
+`
+const Wrapper = styled.div`
+    width: 95%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin-bottom: 15px;
+  /* background-color: red; */
+
+  @media screen and (max-width: 768px) {
+    width: 90%;
+  }
+`
+const Container = styled.div`
+    width: 100%;
+    display: flex;
+`
