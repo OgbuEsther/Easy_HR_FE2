111b1faf73ad425b4da9adbcc26bd3648ddbf8c3
Okwoli-Godwin <okwolig60@gmail.com>
2023-05-11 05:53:11 +0100

    work day setting done

--- src/pages/Settingspage.tsx
+++ src/pages/Settingspage.tsx
@@ -5,17 +5,122 @@ const Settingspage = () => {
   return (
       <Container>
           <Wrapper>
-              
+              <Workcard>
+                  <Top><h6>Work Day Settings</h6></Top>
+                  <Choose><h6>Choose work days</h6></Choose>
+                  <Cardhold>
+                <Card>
+                    <Input type='checkbox' /> 
+                    <Day>Monday</Day>
+                      </Card>
+                      
+                      <Card>
+                    <Input type='checkbox' /> 
+                    <Day>Tuesday</Day>
+                      </Card>
+                      
+                      <Card>
+                    <Input type='checkbox' /> 
+                    <Day>Wednesday</Day>
+                      </Card>
+                      
+                      <Card>
+                    <Input type='checkbox' /> 
+                    <Day>Thursday</Day>
+                      </Card>
+                      
+                      <Card>
+                    <Input type='checkbox' /> 
+                    <Day>Friday</Day>
+                </Card>
+
+                <Card>
+                    <Input type='checkbox' /> 
+                    <Day>Saturday</Day>
+                </Card>
+
+                <Card>
+                    <Input type='checkbox' /> 
+                    <Day>Sunday</Day>
+                </Card>
+            </Cardhold>
+              </Workcard>
           </Wrapper>
     </Container>
   )
 }
 
 export default Settingspage
+const Day = styled.div`
+    font-size: 15px;
+    font-weight: 400;
+    line-height: 1.5;
+    color: #495057;
+    cursor: pointer;
+    margin-left: 12px;
+`
+const Input = styled.input`
+    
+`
+const Card = styled.div`
+    width: 140px;
+    height: 40px;
+    align-items: center;
+    padding-left: 10px;
+    display: flex;
+    font-size: .8rem;
+    font-weight: 400;
+    margin-right: 15px;
+    margin-bottom: 15px;
+    line-height: 1.5;
+    color: #495057;
+    background-color: #fff;
+    display: flex;
+    border: 1px solid #ced4da;
+    border-radius: 5px;
+    transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+    cursor: pointer;
+`
+const Cardhold = styled.div`
+    display: flex;
+    width: 100%;
+    flex-wrap: wrap;
+`
+const Choose = styled.div`
+    h6{
+        color: #000;
+     font-size: 1rem;
+     margin-bottom: 0.5rem;
+    font-weight: 500;
+    line-height: 1.2;
+    margin-top: 17px;
+    }
+`
+const Top = styled.div`
+    h6{
+     color: #000;
+     font-size: 19px;
+     margin-bottom: 0.5rem;
+    font-weight: 500;
+    line-height: 1.2;
+    margin-top: 0;
+    }
+`
+const Workcard = styled.div`
+    width: 500px;
+    display: flex;
+    background: hsla(0,0%,76.9%,.25);
+    color: #4a5568;
+    padding: 16px;
+    border-radius: 5px;
+    margin-top: 40px;
+    border: 1px solid #d3d3d3;
+    flex-direction: column;
+    /* flex-wrap: wrap; */
+`
 
 const Wrapper = styled.div`
   width: 95%;
-  height: 40px;
   display: flex;
   flex-direction: column;
   align-items: center;
