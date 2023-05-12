bbd301f4a31aaabcc957c84243fe0fddba322324
Okwoli-Godwin <okwolig60@gmail.com>
2023-05-12 18:21:13 +0100

    hero page update

--- src/pages/landingpage/Home.tsx
+++ src/pages/landingpage/Home.tsx
@@ -22,19 +22,19 @@ const Superhero = () => {
   
   return (
     <div>
-      {/* <Slider {...settings}> */}
+      <Slider {...settings}>
       <div className='w-full  bg-hero flex justify-center overflow-hidden bg-no-repeat bg-center bg-cover'>
-          <div className='w-full pb-28 bg-black bg-opacity-50 flex-col items-center flex xmd:justify-center xmd:flex xmd:items-center'>
+          <div className='w-full pb-36 bg-black bg-opacity-50 flex-col items-center flex xmd:justify-center xmd:flex xmd:items-center'>
             <div className='mt-56 w-11/12 text-white xmd:mt-36'>
             <Fade
               duration={2000}
             >
-              <div className='w-3/12 xmd:w-full text-6xl lg:w-full font-bold xmd:text-5xl xmd:text-center '>
-               EMPLOYEE PERFORMANCE MANAGEMENT
+              <div className='w-3/12 xmd:w-full text-6xl lg:w-full font-bold xmd:text-4xl xmd:text-center '>
+               Employee Performance Management
               </div>
             </Fade>
             <Fade direction="down">
-              <p className='text-white text-2xl mt-3 xmd:text-center sm:text-2xl'>to take your business further</p>
+              <p className='text-white text-2xl mt-3 xmd:text-center sm:text-sm'>to take your business further</p>
             </Fade>
 
             <div className='xmd:w-full xmd:flex xmd:justify-center xmd:items-center'>
@@ -50,25 +50,27 @@ const Superhero = () => {
       </div>
       </div>
 
-      {/* <div className='w-full bg-righthero flex justify-center overflow-hidden bg-no-repeat bg-center bg-cover'>
-      <div className='w-full pb-28 bg-black bg-opacity-50 flex-col items-center flex xmd:justify-center xmd:flex xmd:items-center '>
+      <div className='w-full bg-righthero flex justify-center overflow-hidden bg-no-repeat bg-center bg-cover'>
+      <div className='w-full pb-36 bg-black bg-opacity-50 flex-col items-center flex xmd:justify-center xmd:flex xmd:items-center '>
         <div className='mt-56 w-11/12 text-white xmd:mt-36'>
             <Fade
               duration={3000}
             >
-              <div className='w-6/12 xmd:w-full text-8xl font-bold xmd:text-5xl xmd:text-center sm:text-3xl'>
-                Payroll & HR Solutions
+              <div className='w-6/12 xmd:w-full text-6xl font-bold xmd:text-4xl xmd:text-center sm:text-3xl'>
+                Employee Performance Management
               </div>
             </Fade>
             <Fade direction="down">
-              <p className='text-white text-3xl mt-3 xmd:text-center sm:text-2xl'>to take your business further</p>
+              <p className='text-white text-3xl mt-3 xmd:text-center sm:text-sm'>to take your business further</p>
             </Fade>
 
             <div className='xmd:w-full xmd:flex xmd:justify-center xmd:items-center'>
               <Fade direction="up">
-              <button className='w-48 h-12 text-white bg-hover flex justify-center items-center font-semibold rounded-3xl hover:bg-white hover:text-hover cursor-pointer transition-all duration-150 ease-in-out delay-150 mt-10'>
+              <NavLink to="/sign-up-option">
+                  <button className='w-48 h-12 text-white bg-hover flex justify-center items-center font-semibold rounded-3xl hover:bg-white hover:text-hover cursor-pointer transition-all duration-150 ease-in-out delay-150 mt-10'>
               Get started
             </button>
+              </NavLink>
             </Fade>
             </div>
             </div>
@@ -76,30 +78,32 @@ const Superhero = () => {
       </div>
 
       <div className='w-full bg-lefthero flex justify-center overflow-hidden bg-no-repeat bg-center bg-cover'>
-          <div className='w-full pb-28 bg-black bg-opacity-50 flex-col items-center flex xmd:justify-center xmd:flex xmd:items-center '>
+          <div className='w-full pb-36 bg-black bg-opacity-50 flex-col items-center flex xmd:justify-center xmd:flex xmd:items-center '>
             <div className='mt-56 w-11/12 text-white xmd:mt-36'>
             <Fade
               duration={3000}
             >
-              <div className='w-6/12 xmd:w-full text-8xl font-bold xmd:text-5xl xmd:text-center sm:text-3xl'>
-                Payroll & HR Solutions
+              <div className='w-6/12 xmd:w-full text-6xl font-bold xmd:text-4xl xmd:text-center sm:text-4xl'>
+                Employee Performance Management
               </div>
             </Fade>
             <Fade direction="down">
-              <p className='text-white text-3xl mt-3 xmd:text-center sm:text-2xl'>to take your business further</p>
+              <p className='text-white text-3xl mt-3 xmd:text-center sm:text-sm'>to take your business further</p>
             </Fade>
 
             <div className='xmd:w-full xmd:flex xmd:justify-center xmd:items-center'>
               <Fade direction="up">
-              <button className='w-48 h-12 text-white bg-hover flex justify-center items-center font-semibold rounded-3xl hover:bg-white hover:text-hover cursor-pointer transition-all duration-150 ease-in-out delay-150 mt-10'>
+              <NavLink to="/sign-up-option">
+                  <button className='w-48 h-12 text-white bg-hover flex justify-center items-center font-semibold rounded-3xl hover:bg-white hover:text-hover cursor-pointer transition-all duration-150 ease-in-out delay-150 mt-10'>
               Get started
             </button>
+              </NavLink>
             </Fade>
             </div>
             </div>
       </div>
-      </div> */}
-    {/* </Slider> */}
+      </div>
+    </Slider>
     </div>
   )
 }
