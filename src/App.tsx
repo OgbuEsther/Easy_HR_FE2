c1b7bbc624e3536ea1db5a2e7a257b4442b285e8
Okwoli-Godwin <okwolig60@gmail.com>
2023-05-30 23:15:45 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { Elements } from './routes/Routes';
-import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
 
 
 const App = () => {
-  const [colorScheme, setColorScheme] = React.useState<ColorScheme>('light');
-  const toggleColorScheme = (value?: ColorScheme) =>
-    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
   return (
     <div>
-       <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-      <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
        <RouterProvider router={Elements} />
-       </MantineProvider>
-    </ColorSchemeProvider>
     </div>
   );
 }
